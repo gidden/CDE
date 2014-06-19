1a773d5afa61e7e90a633745477b45c4b460928f
Matthew Gidden <matthew.gidden@gmail.com>
2014-06-19 12:59:27 -0500

    added scriptability

--- install.py
+++ install.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
 import os
 import shutil
 
-if __name__ == '__main__':
+def main():
     base = os.path.dirname(os.path.abspath(__file__))
     files = ['cde']
     dest = os.path.join(os.environ['HOME'], '.local', 'bin')
     for f in files:
-        shutil.copy(os.path.join(base, f), dest)
+        ffrom = os.path.join(base, f)
+        fto = dest
+        print("Copying {0} to {1}".format(ffrom, fto))
+        shutil.copy(ffrom, fto)
+
+if __name__ == '__main__':
+    main()
